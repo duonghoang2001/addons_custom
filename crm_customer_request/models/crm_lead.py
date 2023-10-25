210eb1096104c9acafbdafc69d7b389c2a037545
Duong Hoang <hhduong2001@gmail.com>
2023-10-25 14:20:01 +0700

    [FIX] redesign compute functions to loop through each lead

--- crm_customer_request/models/crm_lead.py
+++ crm_customer_request/models/crm_lead.py
@@ -17,18 +17,21 @@ class Lead(models.Model):
     
     #@api.depends('crm_customer_request.qty')
     def _compute_total_sale(self):
-        self.total_sale = sum([request.qty for request in self.request_ids])
+        for lead in self:
+            lead.total_sale = sum([request.qty for request in lead.request_ids])
 
     #@api.depends('crm_customer_request.qty', 'crm_customer_request.product_id', 'product_id.list_price')
     def _compute_total_expected_revenue(self):
-        revenues = [request.qty * request.product_id.list_price for request in self.request_ids]
-        self.total_expected_revenue = sum(revenues)
+        for lead in self:
+            revenues = [request.qty * request.product_id.list_price for request in lead.request_ids]
+            lead.total_expected_revenue = sum(revenues)
 
     def _compute_is_new_stage(self):
-        #FIXME: check if current stage is 'New'
-        new_stage_id = self.env.ref('crm.stage_lead1').id
-        record_id = self.env.ref('crm.crm_lead_view_form').id
-        #print(new_stage_id, record_id)
-        self.is_new_stage = new_stage_id == 1
+        #FIXME: check whether current stage is 'New'
+        for lead in self:
+            new_stage_id = lead.env.ref('crm.stage_lead1').id
+            record_id = lead.env.ref('crm.crm_lead_view_form').id
+            print('Debug:', new_stage_id, record_id)
+            lead.is_new_stage = new_stage_id == 1
     
         

54d3ae4a6e3aede83ee02b9e7e80efab7d4a619a
Duong Hoang <hhduong2001@gmail.com>
2023-10-26 14:11:13 +0700

    [ADD] push request info to quotation

--- crm_customer_request/models/crm_lead.py
+++ crm_customer_request/models/crm_lead.py
@@ -5,7 +5,8 @@ from odoo import models, fields, api
 class Lead(models.Model):
     _inherit = 'crm.lead'
 
-    request_ids = fields.One2many('crm.customer.request', 'opportunity_id', string='Requests')
+    request_ids = fields.One2many('crm.customer.request', 'opportunity_id', 
+                                  string='Requests')
     total_sale = fields.Float(string='Total Sale', compute='_compute_total_sale', 
                               store=True)
     total_expected_revenue = fields.Monetary(string='Total Expected Revenue', 
@@ -32,4 +33,15 @@ class Lead(models.Model):
             new_stage_id = lead.env.ref('crm.stage_lead1').id
             lead.is_new_stage = lead.stage_id.id == new_stage_id
     
-        
+    #@api.depends('crm_customer_request.product_id', 'crm_customer_request.qty')
+    def create_quota(self):
+        #TODO: call it whenever user adds item to Opportunity tab
+        for lead in self:
+            order_lines = [{'product_template_id': request.product_id, 
+                     'product_uom_qty': request.qty} 
+                     for request in lead.request_ids]
+            order_values = {
+                'partner_id': lead.partner_id.id,
+                'order_line': order_lines
+            }
+            self.env['sale.order'].create(order_values)

8db6be975a8d47bd96e3ae269fee221ebc41746b
Duong Hoang <hhduong2001@gmail.com>
2023-11-01 10:04:44 +0700

    [FIX] add requests to the API created lead

--- crm_customer_request/controllers/controllers.py
+++ crm_customer_request/controllers/controllers.py
@@ -24,21 +24,27 @@ class CrmCustomerRequest(http.Controller):
         # Expected closing date
         if kw['date_deadline']:
             new_lead['date_deadline'] = datetime.datetime.strptime(kw['date_deadline'], '%d/%m/%Y').date()
-        
+
+        # Create lead
+        new_lead_id = request.env['crm.lead'].sudo().create(new_lead).id
+        lead = request.env['crm.lead'].sudo().search([('id', '=', new_lead_id)])
+        print('Created Lead:', lead)
+
         # Requests
-        new_lead['request_ids'] = []
+        request_lines = []
         for i in range(len(kw['requests'])):
             product_id = request.env['product.template'].sudo().search(
                     [('name', '=', kw['requests'][i]['product'])], limit=1).id
             if product_id:
-                new_lead['request_ids'].append(tuple({
+                request_line = {
                     'product_id': product_id,
                     'qty': kw['requests'][i]['quantity'],
-                    'date': datetime.datetime.strptime(kw['requests'][i]['date'], '%d/%m/%Y').date()
-                }))
-
-        # Create lead
-        new_lead_id = request.env['crm.lead'].sudo().create(new_lead).id
-        print('Created Lead:', request.env['crm.lead'].sudo().search([('id', '=', new_lead_id)]))
+                }
+                if kw['requests'][i]['date']:
+                    request_line['date'] = datetime.datetime.strptime(kw['requests'][i]['date'], '%d/%m/%Y').date() 
+                request_lines.append((0, 0, request_line))
+        lead.update({
+            'request_ids': request_lines
+        })
 
         return json.dumps({'Result': 'Completed'}) 
